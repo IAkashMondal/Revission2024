1f938cfb3c2e6b3a41f3faf68cad35eb185af965
IAkashMondal <adas085322gmail.com>
2024-01-16 13:31:44 +0530

    taking data from terminal and ALSO ADDing data, updateing a new data, and deleting the document too

--- Back_end/day-5-input-taking/index.js
+++ Back_end/day-5-input-taking/index.js
@@ -1,19 +1,19 @@
-// Importing the 'fs' module for file system operations
 const fs = require("fs");
 
 // Retrieving command-line arguments
 const input = process.argv;
 
-// Defining boolean variables to check the command type [add/update/remove]
-const InputAdd = input[2] === "add" || input[2] === "ADD" || input[2] === "Add";
-const InputRemove =
-  input[2] === "remove" || input[2] === "Remove" || input[2] === "REMOVE";
-const InputUpdate =
-  input[2] === "update" || input[2] === "UPDATE" || input[2] === "Update";
+// Convert the command to lowercase for case-insensitive comparison
+const lowerCaseCommand = input[2].toLowerCase();
 
-// Checking the command type to take action
+// Checking the command type
+const InputAdd = lowerCaseCommand === "add";
+const InputRemove = lowerCaseCommand === "remove";
+const InputUpdate = lowerCaseCommand === "newdata";
+
+// Handling different commands
 if (InputAdd) {
-  // If the command is 'add', write the specified data to the file, appending a newline
+  // If the command is 'add', creating a new file ton add data if the file didn't exits but if exits the it will delete all data and set the file to null
   fs.writeFileSync(input[3], input[4] + "\n");
 } else if (InputUpdate) {
   // If the command is 'update', append the specified data to the file, also appending a newline
@@ -22,6 +22,5 @@ if (InputAdd) {
   // If the command is 'remove', delete the specified file
   fs.unlinkSync(input[3]);
 } else {
-  // If the command is not recognized, print an error message
   console.log("Invalid command");
 }
